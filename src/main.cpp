641fb5d5490d8e6935216dc59a45b0dde5101a5d
ccn <ccn@cuppajoeman.com>
2025-07-24 18:17:45 -0400

    feat: start work on string function invocation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+
 #include "graphics/colors/colors.hpp"
 #include "graphics/shader_standard/shader_standard.hpp"
 #include "graphics/vertex_geometry/vertex_geometry.hpp"
+
 #include "system_logic/toolbox_engine/toolbox_engine.hpp"
+#include "utility/meta_utils/meta_utils.hpp"
 
 // https://chatgpt.com/c/68808a85-ca34-8007-8693-5dd3e7510abf
 // continue working on custom vp file type
 
+int add(int x, int y) { return x + y; }
+int mul(int x, int y) { return x * y; }
+
+std::optional<std::string> f(const std::string &input) {
+    std::regex re(R"(^\s*add\s*\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)\s*$)");
+    std::smatch match;
+    if (!std::regex_match(input, match, re))
+        return std::nullopt;
+
+    int x = std::stoi(match[1]);
+    int y = std::stoi(match[2]);
+
+    int result = add(x, y);
+    return std::to_string(result);
+}
+
 int main() {
+
+    std::cout << generate_invoker("int add(int x, int y)") << std::endl;
+    std::cout << f("add(8, 3)").value() << std::endl;
 
     std::unordered_map<SoundType, std::string> sound_type_to_file = {
         {SoundType::UI_HOVER, "assets/sounds/hover.wav"}, {SoundType::UI_CLICK, "assets/sounds/click.wav"},
